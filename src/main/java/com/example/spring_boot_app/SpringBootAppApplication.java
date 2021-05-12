7bf69d39551232914116d024b0dfa70853628aee
Just1lz <exelent.pride@rambler.ru>
2021-05-12 23:50:53 +0700

    custom properties

--- src/main/java/com/example/spring_boot_app/SpringBootAppApplication.java
+++ src/main/java/com/example/spring_boot_app/SpringBootAppApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 
 //@SpringBootApplication //включает в себя 3 аннотации указанные ниже
 @SpringBootConfiguration
 @ComponentScan
 @EnableAutoConfiguration
+@PropertySource("classpath:custom.properties")
 public class SpringBootAppApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/spring_boot_app/times/TimeProvider.java
+++ src/main/java/com/example/spring_boot_app/times/TimeProvider.java
@@ -1,13 +1,22 @@
 package com.example.spring_boot_app.times;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.logging.Logger;
 
 @Component
 public class TimeProvider implements CommandLineRunner {
 
-    private TimeService timeService;
+    private final TimeService timeService;
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @Value("${custom.welcomeMessage}")
+    private String welcomeMessage;
 
     @Autowired
     public TimeProvider(TimeService timeService) {
@@ -16,6 +25,8 @@ public class TimeProvider implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        Logger.getLogger(TimeProvider.class.getName()).info("running " + appName);
+        Logger.getLogger(TimeProvider.class.getName()).info("running " + welcomeMessage);
         this.timeService.printCurrentTime();
     }
 }
